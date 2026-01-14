eb3698922c212f995cdb715cc50df41186fae8ae
Sideways-Sky <anson.eyre@gmail.com>
2026-01-14 14:46:02 -0700

    Env Var port

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
+const PORT = Bun.env.PORT ?? 80
+
 Bun.serve({
-    port: 80,
+	port: PORT,
 
-    fetch(req) {
-        const url = new URL(req.url);
+	fetch(req) {
+		const url = new URL(req.url)
 
-        // Example API route
-        // if (url.pathname === "/api/hello") {
-        //     return Response.json({ message: "Hello from Bun!" });
-        // }
+		// Example API route
+		// if (url.pathname === "/api/hello") {
+		//     return Response.json({ message: "Hello from Bun!" });
+		// }
 
-        // Serve static files
-        const filePath = url.pathname === "/" ? "/index.html" : url.pathname;
+		// Serve static files
+		const filePath = url.pathname === '/' ? '/index.html' : url.pathname
 
-        const file = Bun.file(`./public${filePath}`);
+		const file = Bun.file(`./public${filePath}`)
 
-        return file.exists()
-            ? new Response(file)
-            : new Response("Not found", { status: 404 });
-    },
-});
+		return file.exists()
+			? new Response(file)
+			: new Response('Not found', { status: 404 })
+	},
+})
 
 // For the machine running this, it also means we can access it at that addr
-console.log("Server running at http://localhost:80");
+console.log(`Server running at http://0.0.0.0:${PORT}`)
