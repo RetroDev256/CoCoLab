a411addb5cae988261ffc075074337fc607441b6
Eric Petersen <ericpetersen3.14@gmail.com>
2026-01-14 20:52:26 -0700

    tweak server.js to be easier to work with

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-const PORT = Bun.env.PORT ?? 80
+// PORT=[port here] bun run server.js
+const PORT = Bun.env.PORT ?? 3000;
 
 Bun.serve({
-	port: PORT,
+    port: PORT,
 
-	fetch(req) {
-		const url = new URL(req.url)
+    fetch(req) {
+        const url = new URL(req.url);
 
-		// Example API route
-		// if (url.pathname === "/api/hello") {
-		//     return Response.json({ message: "Hello from Bun!" });
-		// }
+        // Example API route
+        // if (url.pathname === "/api/hello") {
+        //     return Response.json({ message: "Hello from Bun!" });
+        // }
 
-		// Serve static files
-		const filePath = url.pathname === '/' ? '/index.html' : url.pathname
+        // Serve static files
+        const path = url.pathname === "/" ? "/index.html" : url.pathname;
 
-		const file = Bun.file(`./public${filePath}`)
+        const file = Bun.file(`./public${path}`);
 
-		return file.exists()
-			? new Response(file)
-			: new Response('Not found', { status: 404 })
-	},
-})
+        return file.exists()
+            ? new Response(file)
+            : new Response("Not found", { status: 404 });
+    },
+});
 
 // For the machine running this, it also means we can access it at that addr
-console.log(`Server running at http://0.0.0.0:${PORT}`)
+console.log(`Server running at http://localhost:${PORT}`);
