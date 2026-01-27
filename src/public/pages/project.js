c4d544d1120bf095659e53971222336f04e97afb
Maddie Smith :) <maddiemunch123@gmail.com>
2026-01-27 16:48:45 -0700

    Added JS files that I felt would be needed with basic details in each about the functionality of the corresponding page

---
+++ src/public/pages/project.js
@@ -0,0 +1,12 @@
+//This is the logic behind a project, regardless of which project it is
+//this page will need to get the project id and use that to grab information from the server(?)
+//and use it to populate the page
+
+//There should be some users that are clickable, leading to that user's "profile" page
+//For regular people viewing a project, there should be a button they can click that allows them to "join" the project
+//That button will send the user's contact information to the project owner, who can then accept/reject the person
+
+//For project owner, there should be a button that will allow them to mark the project as complete.
+//They can record completion details and submit, which will save information and change project status to completed,
+//thus removing the project from the projectBoard. It will still be visible for the project owner and the other
+//contributors on their personal profiles (and/or the projectManager)
