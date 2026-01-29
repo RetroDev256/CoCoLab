7295faeb8a5a2e69a4a44a29125009b73eac2a57
Maddie Smith :) <maddiemunch123@gmail.com>
2026-01-29 16:37:41 -0700

    Added more notes and html for project.js and associated files

--- src/public/pages/project.js
+++ src/public/pages/project.js
@@ -2,11 +2,18 @@
 //this page will need to get the project id and use that to grab information from the server(?)
 //and use it to populate the page
 
+//SHould there be one lump call to the database for everything about this project? Or should we make a little one for each thing?
+
+//Following will need to be populated using that data:
+//Project title
+//User who created it, along with a link to that user's profile
+//array of tags associated with this project
+//Details about this project
+//The deadline for this project, if there is one
+//How many people are needed to help, and how many of those have already been filled
+//(For regular user, should they be able to see the users associated with a project? Or should that just be up to a project owner?)
+//Clicking join this project will send the current user's contact info to the owner of the project
+
 //There should be some users that are clickable, leading to that user's "profile" page
 //For regular people viewing a project, there should be a button they can click that allows them to "join" the project
 //That button will send the user's contact information to the project owner, who can then accept/reject the person
-
-//For project owner, there should be a button that will allow them to mark the project as complete.
-//They can record completion details and submit, which will save information and change project status to completed,
-//thus removing the project from the projectBoard. It will still be visible for the project owner and the other
-//contributors on their personal profiles (and/or the projectManager)
