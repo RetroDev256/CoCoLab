39c0e75367325d3ea41a89680f2658f68b9f7d63
Sideways-Sky <anson.eyre@gmail.com>
2026-01-30 13:16:01 -0700

    server update

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import sitePlan from './public/pages/sitePlan.html'
 import userSettings from './public/pages/userSettings.html'
 import projectBoard from './public/pages/projectBoard.html'
 import projectManager from './public/pages/projectManager.html'
+import forms from './public/pages/forms.html'
 
 // PORT=[port here] bun run server.js
 const PORT = Bun.env.PORT ?? 3000
@@ -23,13 +24,38 @@ Bun.serve({
 		'/aboutDevs': aboutDevs,
 		'/contactUs': contactUs,
 		'/sitePlan': sitePlan,
+		'/forms': forms,
 		'/userSettings': userSettings,
 		'/projectBoard': projectBoard,
 		'/projectManager': projectManager,
-	},
+		'/api/*': {
+			POST: async (req) => {
+				const form_data = await req.formData()
 
-	fetch: async (req) => {
-		console.log(req.url)
+				try {
+					const api_response = await apiPost(req.url, form_data)
+					if (api_response) return api_response
+				} catch (err) {
+					// Just in case we have out-of-band errors, like for SQL
+					console.log(`POST error encountered: ${err} --- FORM DATA:`)
+					// Log the offending form data for debugging
+					for (const [key, value] of form_data.entries())
+						console.log(JSON.stringify({ key, value }))
+					return Response.json(err, { status: 500 })
+				}
+			},
+			GET: async (req) => {
+				try {
+					// API endpoints for fetching data from each SQL table
+					const api_response = await apiGet(req.url)
+					if (api_response) return api_response
+				} catch (err) {
+					// Just in case we have out-of-band errors, like for SQL
+					console.log(`GET error encountered: ${err}`)
+					return Response.json(err, { status: 500 })
+				}
+			},
+		},
 	},
 })
 
