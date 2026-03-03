f57b113be0777195f541130acbddaa5875753cf1
Sideways-Sky <anson.eyre@gmail.com>
2026-03-02 23:21:26 -0700

    better project page

--- src/public/main.js
+++ src/public/main.js
@@ -1,58 +1,15 @@
-// --------------------------------------------------------------------- BURGER
-
-const burger = document.getElementById("burger");
-const side_menu = document.getElementById("side_menu");
-
-export function openMenu() {
-    side_menu.classList.add("open");
-}
-
-export function closeMenu() {
-    side_menu.classList.remove("open");
-}
-
-if (burger !== null) {
-    // The menu can be opened if and only if the burger is clicked
-    burger.addEventListener("click", (e) => openMenu());
-
-    // The menu can be closed if we did not click on the burger
-    document.addEventListener("click", (event) => {
-        if (!burger.contains(event.target)) closeMenu();
-    });
-
-    // The menu can be closed if we pressed escape
-    document.addEventListener("keydown", (e) => {
-        if (e.key === "Escape") closeMenu();
-    });
-}
-
 // ----------------------------------------------------------- DATABASE HELPERS
 
+const url = "https://coco.alloc.dev";
 // Returns a list of all JS objects in a table
-export async function selectByTable(table) {
-    const url = "https://coco.alloc.dev";
+export async function selectTable(table) {
     const path = `${url}/API/SELECT/${table}`;
     return await (await fetch(path)).json();
 }
 
 // Returns either a single JS object, or null
 export async function selectById(table, id) {
-    const url = "https://coco.alloc.dev";
     const path = `${url}/API/SELECT/${table}/${id}`;
-    const response = await fetch(path);
-    const json = await response.json();
-
-    if (json.length === 0) {
-        return null;
-    } else {
-        return json[0];
-    }
-}
-
-// Returns either a single JS object, or null
-export async function deleteById(table, id) {
-    const url = "https://coco.alloc.dev";
-    const path = `${url}/API/DELETE/${table}/${id}`;
     const response = await fetch(path);
     const json = await response.json();
 
@@ -65,28 +22,66 @@ export async function deleteById(table, id) {
 
 // Returns a list based on a table, field, and value
 export async function selectByValue(table, field, value) {
-    const url = "https://coco.alloc.dev";
     const path = `${url}/API/SELECT/${table}/${field}/${value}`;
     return await (await fetch(path)).json();
 }
 
-// Returns a list based on a table, field, and value
-export async function deleteByValue(table, field, value) {
-    const url = "https://coco.alloc.dev";
-    const path = `${url}/API/DELETE/${table}/${field}/${value}`;
-    return await (await fetch(path)).json();
+// Returns either a single JS object, or null
+export async function deleteById(table, id) {
+    const path = `${url}/API/${table}/${id}`;
+    const response = await fetch(path, { method: "DELETE" });
+    const json = await response.json();
+
+    if (json.length === 0) {
+        return null;
+    } else {
+        return json[0];
+    }
 }
 
-// Used when rendering owners of projects
-export function isValidURL(url) {
-    try {
-        new URL(url);
-        console.log("isValidURL: true");
-        return true;
-    } catch {
-        console.log("isValidURL: false");
-        return false;
-    }
+// Returns a list based on a table, field, and value
+export async function deleteByValue(table, field, value) {
+    const path = `${url}/API/${table}/${field}/${value}`;
+    return await (await fetch(path, { method: "DELETE" })).json();
+}
+
+export async function insert(table, data) {
+    const path = `${url}/API/${table}`;
+    return await (
+        await fetch(path, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+    ).json();
+}
+
+export async function updateById(table, id, data) {
+    const path = `${url}/API/${table}/${id}`;
+    return await (
+        await fetch(path, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+    ).json();
+}
+
+export async function updateByValue(table, field, value, data) {
+    const path = `${url}/API/${table}/${field}/${value}`;
+    return await (
+        await fetch(path, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+    ).json();
 }
 
 // ----------------------------------------------------- AUTHENTICATION HELPERS
