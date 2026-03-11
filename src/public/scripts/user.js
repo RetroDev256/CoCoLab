905832e56b943cacc3f438be9858ed3f7c127ab2
Sideways-Sky <anson.eyre@gmail.com>
2026-03-11 10:27:28 -0600

    Promises and dom rebuilds

--- src/public/scripts/user.js
+++ src/public/scripts/user.js
@@ -1,4 +1,4 @@
-import { getUserId, selectById, selectByValue } from "../main.js";
+import { selectById, selectByValue } from "../main.js";
 
 // Populate the page via repeated API calls - this will increase load times for
 // the server when there is a cache miss, but the complexity of static content
@@ -20,7 +20,7 @@ if (typeof window !== "undefined") {
     populateOwnedProjects(owned);
     populateMemberships(memberships);
     populateRequests(requests);
-    populateTags(memberships);
+    populateRoles(memberships);
     populateUser(user);
 }
 
@@ -29,26 +29,18 @@ function populateOwnedProjects(projects) {
     const div = document.getElementById("owned_projects");
     if (!div) throw new Error("owned projects div does not exist");
 
-    for (const project of projects) {
-        console.log("Adding project...");
-        if (project.completed) {
-            div.innerHTML += `
+    div.innerHTML = projects
+        .map(
+            (project) => `
             <a href="./project.html?id=${project.id}" target="_blank">
                 <div class="p-4 gap-2 rounded-lg bg-base-200 flex justify-between">
                     <span>${project.project_name}</span>
-                    <span class="badge badge-success">Completed</span>
+                    ${project.completed ? '<span class="badge badge-success">Completed</span>' : "<span class='badge badge-primary'>Active</span>"}
+                    
                 </div>
-            </a>`;
-        } else {
-            div.innerHTML += `
-            <a href="./project.html?id=${project.id}" target="_blank">
-                <div class="p-4 gap-2 rounded-lg bg-base-200 flex justify-between">
-                    <span>${project.project_name}</span>
-                    <span class="badge badge-primary">Active</span>
-                </div>
-            </a>`;
-        }
-    }
+            </a>`
+        )
+        .join("");
 }
 
 // Populates the HTML that deals with projects that the user works with
@@ -56,18 +48,22 @@ async function populateMemberships(memberships) {
     const div = document.getElementById("member_projects");
     if (!div) throw new Error("member projects div does not exist");
 
-    for (const membership of memberships) {
-        const project = await selectById("project", membership.project_id);
-        if (!project) throw new Error("membership references void project");
+    Promise.all(
+        memberships.map(async (membership) => {
+            const project = await selectById("project", membership.project_id);
+            if (!project) throw new Error("membership references void project");
 
-        div.innerHTML += `
+            return `
         <a href="./project.html?id=${membership.project_id}" target="_blank">
             <div class="p-4 gap-2 rounded-lg bg-base-200 flex justify-between">
                 <span>${project.project_name}</span>
                 <span class="badge badge-primary">${membership.role}</span>
             </div>
         </a>`;
-    }
+        })
+    ).then((html) => {
+        div.innerHTML = html.join("");
+    });
 }
 
 // Populates the HTML that deals with requests for your project roles
@@ -75,32 +71,35 @@ async function populateRequests(requests) {
     const div = document.getElementById("pending_requests");
     if (!div) throw new Error("pending requests div does not exist");
 
-    for (const request of requests) {
-        const project = await selectById("project", request.project_id);
-        if (!project) throw new Error("request references void project");
+    Promise.all(
+        requests.map(async (request) => {
+            const project = await selectById("project", request.project_id);
+            if (!project) throw new Error("membership references void project");
 
-        div.innerHTML += `
+            return `
         <a href="./project.html?id=${request.project_id}" target="_blank">
             <div class="p-4 gap-2 rounded-lg bg-base-200 flex justify-between">
                 <span>${project.project_name}</span>
                 <span class="badge badge-primary">${request.role}</span>
             </div>
         </a>`;
-    }
+        })
+    ).then((html) => {
+        div.innerHTML = html.join("");
+    });
 }
 
 // Populates the HTML that deals with roles associated with the user
-function populateTags(memberships) {
-    const div = document.getElementById("user_tags");
+function populateRoles(memberships) {
+    const div = document.getElementById("user_roles");
     if (!div) throw new Error("user tags div does not exist");
 
     // De-duplicate the list of roles
     const role_list = new Set();
     for (const membership of memberships) role_list.add(membership.role);
-
-    // Add one tag for the role that we have
-    for (const role of role_list)
-        div.innerHTML += `<span class="badge badge-secondary">${role}</span>`;
+    div.innerHTML = Array.from(role_list).map(
+        (role) => `<span class="badge badge-secondary">${role}</span>`
+    );
 }
 
 // Populates the HTML that relies on the user data
@@ -108,13 +107,24 @@ function populateUser(user) {
     if (!user) throw new Error("missing user object");
 
     const title = document.getElementById("profile-title");
-    const name = document.getElementById("user_name");
-    const joined = document.getElementById("user_joined");
-    const email = document.getElementById("user_email");
+    const div = document.getElementById("profile");
+    if (!div) throw new Error("profile div does not exist");
 
-    const date = new Date(user.created_at);
-    joined.textContent = `Joined ${date.toLocaleDateString()}`;
     title.textContent = `${user.user_name} | Profile`;
-    name.textContent = user.user_name;
-    email.textContent = user.email;
+    div.innerHTML = `<div class="flex items-center gap-4">
+
+        <div class="avatar avatar-placeholder">
+            <div class="bg-neutral text-neutral-content size-20 rounded-full">
+                <span class="text-5xl">${user.user_name.charAt(0).toUpperCase()}</span>
+            </div>
+        </div>
+
+        <div class="space-y-1">
+            <h1 class="text-4xl font-bold" id="user_name">${user.user_name}</h1>
+            <p class="opacity-70" id="user_joined">
+            ${new Date(user.created_at).toLocaleDateString()}
+            </p>
+            <p class="opacity-70" id="user_email">${user.email}</p>
+        </div>
+    </div>`;
 }
